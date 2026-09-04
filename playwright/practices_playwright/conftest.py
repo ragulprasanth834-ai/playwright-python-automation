92f92491cdf1ea9a76e4bd75f489ae297b3231fb
ragulprasanth834-ai <ragulprasanth834@gmail.com>
2026-09-04 06:26:10 +0530

    added additional feaure

--- playwright/practices_playwright/conftest.py
+++ playwright/practices_playwright/conftest.py
@@ -4,5 +4,10 @@ from playwright.sync_api import Page
 
 @pytest.fixture
 def practice_page(page: Page):
-    page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+    page.goto("https://automatewithbipin.com/?utm_source=chatgpt.com")
+    return page
+
+@pytest.fixture
+def practice_page1(page:Page):
+    page.goto("https://automatewithbipin.com/?utm_source=chatgpt.com")
     return page
